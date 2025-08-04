5c9030d1a56ad578b8959b9b8a150f750b0b65e2
Melissa Kariuki <Melissa.Kariuki@imbank.co.ke>
2025-08-04 17:04:09 +0300

    Adding Search Functionality

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -15,10 +15,19 @@ export class Home {
   // Injecting HousingService to access housing locations
   housingLocationList: HousingLocationInfo[] = [];
   housingService = inject(HousingService);
-
+  filteredLocationList: HousingLocationInfo[] = [];
   constructor(){
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
-   
+ filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+  }  
 }
    
