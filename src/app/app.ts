9596356e08a50f87d45dbfe4ae60b9b998cfe556
Melissa Kariuki <Melissa.Kariuki@imbank.co.ke>
2025-08-04 12:20:45 +0300

    adding routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Home} from './home/home';
-
-
+import {Home } from './home/home';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [Home, RouterOutlet],
+  imports: [Home, RouterModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
