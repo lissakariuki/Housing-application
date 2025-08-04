d4c3ff28f9e3181b15851a6affe5e8611fc5460d
Melissa Kariuki <Melissa.Kariuki@imbank.co.ke>
2025-08-04 16:23:07 +0300

    Integrating angular forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -108,7 +108,11 @@ export class HousingService {
         laundry: true,
       },
     ];
-  
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
   getAllHousingLocations(): HousingLocationInfo[] {
     return this.housingLocationList;
   }
